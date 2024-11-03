bdea5e50911e93e30afb59de7cdbae99031ea055
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-11-03 11:55:49 +0530

    Added - Custom color theme

--- app/src/main/java/com/techtribeservices/watchme/ui/theme/Color.kt
+++ app/src/main/java/com/techtribeservices/watchme/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.techtribeservices.watchme.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Orange300 = Color(0xFFFF8F71)
+val Blue200 = Color(0xFF71D6FF)
+val Red200 = Color(0xFFf19a9a)
+val Red500 = Color(0xFFEF2D1A)
+val Red900 = Color(0xFFBA1A1A)
+val BrownRed900 = Color(0xFF8c0808)
+val White50 = Color(0xFFFFFFFF)
+val Gray900 = Color(0xFF211F30)
+val Gray400 = Color(0xFFBBBBBB)
--- app/src/main/java/com/techtribeservices/watchme/ui/theme/Theme.kt
+++ app/src/main/java/com/techtribeservices/watchme/ui/theme/Theme.kt
@@ -12,25 +12,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Orange300,
+    onPrimary = White50,
+    secondary = Blue200,
+    onSecondary = White50,
+    background = Gray900,
+    onBackground = Gray400,
+    error = Red900,
+    onError = White50,
+    errorContainer = Red200,
+    onErrorContainer = BrownRed900
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Orange300,
+    onPrimary = White50,
+    secondary = Blue200,
+    onSecondary = White50,
+    background = Gray900,
+    onBackground = Gray400,
+    error = Red900,
+    onError = White50,
+    errorContainer = Red200,
+    onErrorContainer = BrownRed900
 )
 
 @Composable
